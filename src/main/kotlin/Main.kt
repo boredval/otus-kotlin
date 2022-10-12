eefd995e205da6b8fcd91eab51dbfd120a443c80
Eduard <eduard.borisov94@gmail.com>
2022-10-12 17:46:10 +0300

    Done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,49 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.text.DecimalFormat
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    sumOfDigitsInNumber(825);
+
+    minutesToYearsAndMonths(38578378)
+
+    calculateSquareOfRectangle(10.0, 3.0);
+}
+
+
+fun sumOfDigitsInNumber(number: Int) {
+
+    var temp = number;
+
+    val numbers = arrayListOf<Int>()
+
+    while (temp > 0) {
+        numbers.add(temp % 10)
+        temp /= 10
+    }
+
+    val result = numbers.sum()
+
+    println("сумма чисел в числе $number равна $result")
+}
+
+fun minutesToYearsAndMonths(minutes: Long) {
+
+    val months = (minutes / 43800L);
+    val years = months / 12
+    val remainingMonths = months % 12
+
+    println("$minutes минут равно $years годам и $remainingMonths месяцам")
+
+}
+
+fun calculateSquareOfRectangle(width: Double , height: Double) {
+
+    val result: Double;
+
+    result = sqrt(width.pow(2.0) + height.pow(2.0))
+
+    val numberFormat = DecimalFormat("#.000")
+    println(numberFormat.format(result))
 }
