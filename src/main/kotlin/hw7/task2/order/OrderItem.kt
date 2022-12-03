c6b4871840303e0d3ca77beca33835719b4c3074
Eduard <eduard.borisov94@gmail.com>
2022-12-03 16:09:07 +0300

    Revert "hw7"
    This reverts commit 1a41e882a809e6e1aaedf4e7ea89738bde7dbe61.

--- src/main/kotlin/hw7/task2/order/OrderItem.kt
+++
@@ -1,4 +0,0 @@
-package ru.otus.hw_7.task_2.order
-
-data class OrderItem(val name: String, val weight: Double, val price: Int) {
-}
--- src/main/kotlin/hw7/task2/payment/CashPayment.kt
+++
@@ -1,9 +0,0 @@
-package ru.otus.hw_7.task_2.payment
-
-import hw7.task2.enum.Currency
-
-class CashPayment(val total: Int, val change: Int, amount: Int, currency: Currency) : Payment(amount, currency) {
-
-    override fun performPayment() {
-    }
-}
